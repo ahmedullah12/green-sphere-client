ad665b57d9cfd3cba69389e6ef920e26b20fad6a
Ahmed Ullah <gulamjoy321@gmail.com>
2024-10-06 21:13:45 +0600

    protected route added

--- src/constant.ts
+++ src/constant.ts
@@ -1,6 +1,10 @@
 export const protectedRoutes = [
+  "/news-feed",
+  "/news-feed/:page*",
   "/profile",
   "/profile/:page*",
+  "/user-dashboard",
+  "/admin-dashboard",
   "/login",
   "/register",
 ];
